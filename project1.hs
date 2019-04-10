8fb77c2aacae09d4bc10ead50a72113fd88e8ae1
Wenyen Wei <weiwenyen1123@gmail.com>
2019-04-10 22:41:34 +1000

    Update project1.hs

--- project1.hs
+++ project1.hs
@@ -5,10 +5,15 @@ import Data.List
 data Note = Note Char deriving (Show, Eq, Read)
 data Octave = Octave Char deriving (Show, Eq, Read)
 data Pitch = Pitch Note Octave deriving (Show, Eq, Read)
-type GameState = Int
+type GameState = ([[Pitch]],[Pitch])
 
 pitchList :: [Pitch]
-pitchList = [Pitch (Note x) (Octave y) | x <- ['A'..'G'], y <- ['1'..'3']]
+pitchList = makePitchList ['A'..'G'] ['1'..'3']
+
+makePitchList :: [Char] -> [Char] -> [Pitch]
+makePitchList notes [] = [Pitch (Note x) (Octave y) | x <- notes,  y <- ['1'..'3']]
+makePitchList [] octaves = [Pitch (Note x) (Octave y) | x <- ['A'..'G'],  y <- octaves]
+makePitchList notes octaves = [Pitch (Note x) (Octave y) | x <- notes, y <- octaves]
 
 -- rewrite this bit
 combinations :: (Eq t, Num t) => t -> [a] -> [[a]]
@@ -35,25 +40,24 @@ countCorrectPitch :: [String] -> [String] -> Int
 countCorrectPitch target [x] = if x `elem` target then 1 else 0
 countCorrectPitch target (x:xs) = countCorrectPitch target [x] + countCorrectPitch target xs
 
-toNode :: [String] -> [Char]
-toNode [] = []
-toNode (x:xs) = head x:toNode xs
+toNote :: [String] -> [Char]
+toNote [] = []
+toNote (x:xs) = head x:toNote xs
 
 countTrue :: [Bool] -> Int
 countTrue [] = 0
 countTrue (x:xs) = if x then 1+countTrue xs else 0+countTrue xs
 
 countCorrectNote :: [String] -> [String] -> Int
-countCorrectNote arr1 arr2 = countTrue (map (`elem` (toNode arr1)) (toNode arr2))
+countCorrectNote arr1 arr2 = countTrue (map (`elem` (toNote arr1)) (toNote arr2))
 
 toOctave :: [String] -> [Char]
 toOctave [] = []
-toOctave (x:xs) = tail x ++ toOctave xs
+toOctave (x:xs) = nub (tail x ++ toOctave xs)
 
 countCorrectOctave :: [Char] -> [Char] -> Int
-countCorrectOctave [] _ = 0
 countCorrectOctave _ [] = 0
-countCorrectOctave (x:xs) (y:ys) = if (x == y) then 1+countCorrectOctave xs ys else countCorrectOctave (x:xs) ys
+countCorrectOctave target (x:xs) = if x `elem` target then 1 + countCorrectOctave target xs else countCorrectOctave target xs
 
 pitchesToStrings :: [Pitch] -> [String]
 pitchesToStrings [] = []
@@ -62,20 +66,62 @@ pitchesToStrings (x:xs) = pitchToString x:pitchesToStrings xs
 pitchToString :: Pitch -> String
 pitchToString (Pitch (Note x) (Octave y)) = x:[y]
 
-
 -- add toPitch to this function
 feedback :: [Pitch] -> [Pitch] -> (Int,Int,Int)
 feedback _ [] = (0,0,0)
 feedback [] _ = (0,0,0)
 feedback arr1 arr2 = 
-    (countCorrectPitch l1 l2, countCorrectNote l1 l2 - countCorrectPitch l1 l2, countCorrectOctave (toOctave l1) (toOctave l2) - countCorrectPitch l1 l2)
+    (
+      countCorrectPitch l1 l2, 
+      countCorrectNote l1 l2 - countCorrectPitch l1 l2, 
+      countCorrectOctave (toOctave l1) (toOctave l2) - countCorrectPitch l1 l2
+    )
     where l1 = pitchesToStrings arr1
           l2 = pitchesToStrings arr2
 
 initialGuess :: ([Pitch],GameState)
-initialGuess = ([(Pitch (Note 'A') (Octave '1')), (Pitch (Note 'A') (Octave '2')), (Pitch (Note 'A') (Octave '1'))], 0)
+initialGuess = ([
+  (Pitch (Note 'A') (Octave '1')), 
+  (Pitch (Note 'A') (Octave '2')), 
+  (Pitch (Note 'A') (Octave '3'))], (getPitchList,[]))
+
+getUpdatedRemoveList :: [Pitch] -> [Pitch] -> (Int, Int, Int) -> [Pitch]
+getUpdatedRemoveList input removeList feedbackInput = 
+  case feedbackInput of (0, 0, 0) -> nub (removeList ++ (handleTripleZeroCase input))
+                        (0, 0, _) -> nub (removeList ++ (handleZeroNote input))
+                        (0, _, 0) -> nub (removeList ++ (handleZeroOctave input))
+                        (0, _, _) -> nub (removeList ++ input)
+                        feedbackInput -> removeList
 
 nextGuess :: ([Pitch],GameState) -> (Int,Int,Int) -> ([Pitch],GameState)
-nextGuess (input, index) feedbackInput = (getPitchList !! index, index + 1)
+nextGuess (input, (arr, removeList)) feedbackInput = 
+    let updatedRemoveList = getUpdatedRemoveList input removeList feedbackInput
+        updatedArr = jumpToNonRemovePitch arr updatedRemoveList
+    in ((head updatedArr), ((tail updatedArr), updatedRemoveList))
+
+jumpToNonRemovePitch :: [[Pitch]] -> [Pitch] -> [[Pitch]]
+jumpToNonRemovePitch arr rm = 
+  if all (==False) (map (`elem` (head arr)) rm) then arr else jumpToNonRemovePitch (tail arr) rm
 
 
+-- can prob merge these to one
+handleTripleZeroCase :: [Pitch] -> [Pitch]
+handleTripleZeroCase [] = []
+handleTripleZeroCase (x:xs) = nub (generateRemoveItems x ++ handleTripleZeroCase xs)
+
+handleZeroNote :: [Pitch] -> [Pitch]
+handleZeroNote [] = []
+handleZeroNote (x:xs) = generateRemoveItemsByNote x ++ handleZeroNote xs
+
+handleZeroOctave :: [Pitch] -> [Pitch]
+handleZeroOctave [] = []
+handleZeroOctave (x:xs) = generateRemoveItemsByOctave x ++ handleZeroOctave xs
+
+generateRemoveItemsByNote :: Pitch -> [Pitch]
+generateRemoveItemsByNote (Pitch (Note n) _) = nub ((makePitchList [n] []))
+
+generateRemoveItemsByOctave :: Pitch -> [Pitch]
+generateRemoveItemsByOctave (Pitch _ (Octave o)) = nub ((makePitchList [] [o]))
+
+generateRemoveItems :: Pitch -> [Pitch]
+generateRemoveItems (Pitch (Note n) (Octave o)) = nub ((makePitchList [n] []) ++ (makePitchList [] [o]))
