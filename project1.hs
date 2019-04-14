3c8a94bfde43490149518d099d2d95ac599bdaa0
Wenyen Wei <weiwenyen1123@gmail.com>
2019-04-14 21:31:00 +1000

    add doc

--- project1.hs
+++ project1.hs
@@ -1,3 +1,16 @@
+-- Author: Wenyen Wei <wenyenw@student.unimelb.edu.au>
+-- Login Id: 949624
+
+-- Purpose: The objective of this project is to practice and assess my 
+-- understanding of functional programming and Haskell. This project is to 
+-- implement both the guessing and answering parts of a logical guessing game.
+
+-- The program: This is a Musician game, one player is the composer and the 
+-- other is the performer. The composer begins by selecting a three-pitch
+-- musical chord, where each pitch comprises a musical note, one of A, B,
+-- C, D, E, F, or G, and an octave, one of 1, 2, or 3. The player continues
+-- guessing until they got the right pitches.
+
 module Proj1 (Pitch, toPitch, feedback,
 GameState, initialGuess, nextGuess) where
 
@@ -5,52 +18,71 @@ import Data.List
 data Note = Note Char deriving (Show, Eq, Read)
 data Octave = Octave Char deriving (Show, Eq, Read)
 data Pitch = Pitch Note Octave deriving (Show, Eq, Read)
-type GameState = ([[Pitch]],[Pitch])
+-- GameState is a list of remaining 3-Pitche combinations
+type GameState = ([[Pitch]])
+
+toPitch :: String -> Maybe Pitch
+toPitch [] = Nothing
+toPitch [x, y] = if isNote x && isOctave y 
+  then Just (Pitch (Note x) (Octave y)) else Nothing
+
+-- "pitchList", "combinations", "getPitchList" are all functions/supporting
+-- functions for generating pitch combinations from A1 ... G3
 
 pitchList :: [Pitch]
-pitchList = makePitchList ['A'..'G'] ['1'..'3']
+pitchList = [Pitch (Note x) (Octave y) | x <- ['A'..'G'], y <- ['1'..'3']]
 
-makePitchList :: [Char] -> [Char] -> [Pitch]
-makePitchList notes [] = [Pitch (Note x) (Octave y) | x <- notes,  y <- ['1'..'3']]
-makePitchList [] octaves = [Pitch (Note x) (Octave y) | x <- ['A'..'G'],  y <- octaves]
-makePitchList notes octaves = [Pitch (Note x) (Octave y) | x <- notes, y <- octaves]
-
--- rewrite this bit
 combinations :: (Eq t, Num t) => t -> [a] -> [[a]]
-combinations 0 lst = [[]]
-combinations n lst = do
-    (x:xs) <- tails lst
-    rest   <- combinations (n-1) xs
-    return $ x : rest
+combinations 0 arr = [[]]
+combinations n arr = do
+    (x:xs) <- tails arr
+    remain   <- combinations (n-1) xs
+    return ([x] ++ remain)
 
 getPitchList :: [[Pitch]]
 getPitchList = combinations 3 pitchList
 
+
+-- "isNote", "isOctave", "toNote", "toOctave" are functions to validate/
+-- transform pitches these are support functions for "feedback" function
+
 isNote :: Char -> Bool
-isNote x = x == 'A' || x == 'B' || x == 'C' || x == 'D' || x == 'E' || x == 'F' || x == 'G'
+isNote x = 
+  x == 'A' || x == 'B' || x == 'C' || x == 'D' 
+  || x == 'E' || x == 'F' || x == 'G'
 
 isOctave :: Char -> Bool
 isOctave x = x == '1' || x == '2' || x == '3'
-
-toPitch :: String -> Maybe Pitch
-toPitch [] = Nothing
-toPitch [x, y] = if isNote x && isOctave y then Just (Pitch (Note x) (Octave y)) else Nothing
-
-getCorrectPitches :: [String] -> [String] -> [String]
-getCorrectPitches target [] = []
-getCorrectPitches target (x:xs) = if x `elem` target then x:getCorrectPitches target xs else getCorrectPitches target xs
 
 toNote :: [String] -> [Char]
 toNote [] = []
 toNote (x:xs) = nub $ head x:toNote xs
 
-countCorrect :: [Char] -> [Char] -> Int
-countCorrect _ [] = 0
-countCorrect target (x:xs) = if x `elem` target then 1 + countCorrect target xs else countCorrect target xs
-
 toOctave :: [String] -> [Char]
 toOctave [] = []
 toOctave (x:xs) = nub (tail x ++ toOctave xs)
+
+-- "getCorrectPitches", "countCorrect", counts the correct Pitches/Note/Octave
+-- of a Pitch combination. These are support functions for "feedback" function 
+
+-- "getCorrectPitches" count the common pitches between the target and guess
+getCorrectPitches :: [String] -> [String] -> [String]
+getCorrectPitches target [] = []
+getCorrectPitches target (x:xs) = 
+  if x `elem` target 
+    then x:getCorrectPitches target xs 
+    else getCorrectPitches target xs
+
+-- "countCorrect" count the common Notes/Octaves between the target and guess
+countCorrect :: [Char] -> [Char] -> Int
+countCorrect _ [] = 0
+countCorrect target (x:xs) = 
+  if x `elem` target 
+    then 1 + countCorrect target xs 
+    else countCorrect target xs
+
+-- "pitchesToStrings", "pitchToString", "removeFromList" are support 
+-- functions for "feedback" function
 
 pitchesToStrings :: [Pitch] -> [String]
 pitchesToStrings [] = []
@@ -59,6 +91,17 @@ pitchesToStrings (x:xs) = pitchToString x:pitchesToStrings xs
 pitchToString :: Pitch -> String
 pitchToString (Pitch (Note x) (Octave y)) = x:[y]
 
+removeFromList :: [String] -> [String] -> [String]
+removeFromList target [] = []
+removeFromList target (x:xs) = 
+  if x `elem` target 
+    then removeFromList target xs 
+    else x:removeFromList target xs
+
+-- "feedback" first use "getCorrectPitches" function and then remove the 
+-- correct pitches from the list as shown in updateL1/updateL2.
+-- "countCorrect" then use the updated list to process counting the 
+-- correct Octave/Notes.
 feedback :: [Pitch] -> [Pitch] -> (Int,Int,Int)
 feedback _ [] = (0,0,0)
 feedback [] _ = (0,0,0)
@@ -78,12 +121,15 @@ initialGuess :: ([Pitch],GameState)
 initialGuess = ([
   (Pitch (Note 'A') (Octave '1')), 
   (Pitch (Note 'A') (Octave '2')), 
-  (Pitch (Note 'A') (Octave '3'))], (getPitchList,[]))
+  (Pitch (Note 'A') (Octave '3'))], getPitchList)
 
 nextGuess :: ([Pitch],GameState) -> (Int,Int,Int) -> ([Pitch],GameState)
-nextGuess (input, (arr, removeList)) feedbackInput =
+nextGuess (input, arr) feedbackInput =
   let updatedArr = jumpToNonRemovePitch arr input feedbackInput
-  in ((head updatedArr), ((tail updatedArr), removeList))
+  in ((head updatedArr), (tail updatedArr))
+
+-- the strategy of "jumpToNonRemovePitch" is to remove symmetry in the 
+-- problem space.
 
 jumpToNonRemovePitch :: [[Pitch]] -> [Pitch] -> (Int, Int, Int) -> [[Pitch]]
 jumpToNonRemovePitch arr input feedbackInput = 
